eda8a47d03be6937758762cfd78b1b4581ad6544
tonyhle <tonyhieule@yahoo.com>
2016-11-25 17:14:07 -0800

    add file

---
+++ ConsoleApplication4/ConsoleApplication4/Source.cpp
@@ -0,0 +1,11 @@
+//RUN AS ADMIN
+#include <cstdlib>
+using namespace std;
+
+
+int main()
+{
+	system("Robocopy \"//VINACOPRECISION/VinacoServer\" \"C:/Users/Danny's/Google Drive/VinacoServer\" /MIR /Z");
+	system("pause");
+	return 0;
+}
